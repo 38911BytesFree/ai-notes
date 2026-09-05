3191806bb668f02f86833ae7e7187f04d121189a
38911BytesFree <38911BytesFree@users.noreply.github.com>
2026-09-05 09:53:13 +0100

    phase2: remove unused github-token secret

--- infra/terraform/secrets.tf
+++ infra/terraform/secrets.tf
@@ -14,14 +14,3 @@ resource "google_secret_manager_secret_iam_member" "web_session_secret" {
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.web.email}"
 }
-
-resource "google_secret_manager_secret" "github_token" {
-  secret_id = "github-token"
-  project   = var.project_id
-
-  replication {
-    auto {}
-  }
-
-  depends_on = [google_project_service.services["secretmanager.googleapis.com"]]
-}
